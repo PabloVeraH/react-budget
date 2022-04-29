61f1913f376dacae111c6f463128804fc7f10db2
Pablo Vera <pablo.vera82@gmail.com>
2022-04-28 22:05:23 -0400

    Add basic validation

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -4,16 +4,29 @@ const Pregunta = () => {
 
     const [cantidad, guardaCantidad] = useState(0);
 
+    const [error, guardarError] = useState(false);
+
 
     const definirPresupuesto = (e) => {
         guardaCantidad(parseInt(e.target.value, 10))
+    }
+
+    const agregarPresupuesto = (e) => {
+        e.preventDefault();
+
+        if (cantidad < 1 || isNaN(cantidad) ) {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
     }
 
     return ( 
         <Fragment>
             <h2>How much money you have?</h2>
 
-            <form>
+            <form onSubmit={agregarPresupuesto}>
                 <input
                     type="number"
                     className="u-full-width"
